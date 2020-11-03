99e5901650705399e48a17b1db8bf2783370dc1d
brice <brice@MacBook-Pro-de-brice.local>
2020-11-03 15:21:25 +0100

    modif scrollling

--- src/Carrousel.js
+++ src/Carrousel.js
@@ -2,8 +2,8 @@ function Carrousel({ pictures }) {
   return (
     <div className="container">
       <div className="scrolling">
-        {pictures.map((item) => {
-          return <img src={item} alt="movie" />;
+        {pictures.map((item, i) => {
+          return <img className="picture" key={i} src={item} alt="movie" />;
         })}
       </div>
     </div>
